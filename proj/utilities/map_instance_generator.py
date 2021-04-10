45b7c75486f0a7be91c83b3ac5641cf7d1df9e4b
Piotr <bobepiotr@gmail.com>
2021-04-10 11:32:25 +0200

    Add function saving plot to png file

--- proj/utilities/map_instance_generator.py
+++ proj/utilities/map_instance_generator.py
@@ -3,6 +3,8 @@ from scipy.spatial import Delaunay
 import numpy as np
 import random as ran
 from collections import defaultdict
+import os
+import datetime
 
 
 def find_neighbors(tess):
@@ -52,12 +54,30 @@ def create_tri_plot(tri, points):
     plt.show()
 
 
-def create_colored_plot(tri, points, result):
+def display_colored_plot(tri, points, result):
     if result is not None:
         plt.triplot(points[:, 0], points[:, 1], tri.simplices)
         for point, color in result.items():
             plt.plot(point[0], point[1], 'o', c=color, markersize=20)
         plt.show()
+    else:
+        print('Result is failure')
+
+
+def save_colored_plot(tri, points, result):
+    root_path = os.path.dirname(os.path.abspath('__root__'))
+    output_dir_name = datetime.datetime.now().strftime("%d%m%Y_%H%M%S%f")
+    output_path = os.path.join(root_path, 'map_colouring_solutions', output_dir_name)
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    if result is not None:
+        for i, res in enumerate(result):
+            plt.triplot(points[:, 0], points[:, 1], tri.simplices)
+            for point, color in res.items():
+                plt.plot(point[0], point[1], 'o', c=color, markersize=20)
+            plt.savefig(output_path+'\\figure'+str(i)+'.png')
+            plt.clf()
     else:
         print('Result is failure')
 
